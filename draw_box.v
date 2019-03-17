6e0b49f38e2b8521eeba0841ba203791a522f189
saadabbas98 <saadabbas.shah@mail.utoronto.ca>
2019-03-17 17:07:55 -0400

    Add files via upload

--- draw_box.v
+++ draw_box.v
@@ -1,26 +1,23 @@
-module draw_box(	givex, 
-	       	givey,
-			size,
-		x,
-		y);
+module draw_box(givex, givey, size, x, y);
 
-input givex;
-input givey;
-input size;
-output reg x; //if does not work try removing reg
-output reg y; //if does not work remove reg
+	input givex;
+	input givey;
+	input size;
+	output reg x; //if does not work try removing reg
+	output reg y; //if does not work remove reg
 
-x <= givex;
-y <= givey;
-always*()
-begin
-	for( index = 0; index < size; index = index + 1)
+	assign x <= givex;
+	assign y <= givey;
+	always*()
 	begin
-		for(subI = 0; subI < size ; subI = subI + 1)
+		for( index = 0; index < size; index = index + 1)
 		begin
-			x <= x + 1;
+			for(subI = 0; subI < size ; subI = subI + 1)
+			begin
+				x <= x + 1;
+			end
+			y <= givey + 1;
+			x <= givex;
 		end
-		y <= givey + 1;
-		x <= givex;
 	end
-end
+endmodule
--- start_state.v
+++ start_state.v
@@ -5,24 +5,22 @@ module state_one(	set,
 					);
 // this module draws the boards on the box 
 
-input CLOCK_50;
-reg set;
-output reg x;
-output reg y;
+	input CLOCK_50;
+	reg set;
+	output reg x;
+	output reg y;
 
-always(posedge, negedge);
-begin 
-
-if(set == 2b'11)
-begin
-	x <= 0;
-	y<= 256/2 - 10
-	set <= 2b'10
-end
-else if(set == 2b'10)
-begin
-	x<=256;
-	y<=256/2-10;
-	set<= 00;
-end
+	always @(posedge CLOCK_50, negedge CLOCK_50)
+	begin 
+		if(set == 2b'11) begin
+			x <= 0;
+			y<= 256/2 - 10;
+			set <= 2b'10;
+		end
+		else if(set == 2b'10) begin
+			x<=256;
+			y<=256/2-10;
+			set<= 00;
+		end
+	end
 endmodule
