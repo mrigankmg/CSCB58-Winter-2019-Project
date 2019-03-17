cbe52404682b82d79137003c130e18c140c3b0c0
prakharS07 <48506991+prakharS07@users.noreply.github.com>
2019-03-17 16:16:23 -0400

    Add files via upload

---
+++ draw_box.v
@@ -0,0 +1,26 @@
+module draw_box(	givex, 
+	       	givey,
+			size,
+		x,
+		y);
+
+input givex;
+input givey;
+input size;
+output reg x; //if does not work try removing reg
+output reg y; //if does not work remove reg
+
+x <= givex;
+y <= givey;
+always*()
+begin
+	for( index = 0; index < size; index = index + 1)
+	begin
+		for(subI = 0; subI < size ; subI = subI + 1)
+		begin
+			x <= x + 1;
+		end
+		y <= givey + 1;
+		x <= givex;
+	end
+end
