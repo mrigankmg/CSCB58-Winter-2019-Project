f0bdb3d25205aafe94082566e520c9b9c142d1a3
prakharS07 <48506991+prakharS07@users.noreply.github.com>
2019-03-18 14:33:59 -0400

    Add files via upload

---
+++ upstate.v
@@ -0,0 +1,32 @@
+module upstate(atTop,
+				player,
+				moved,
+				givey
+					x,
+					y);
+
+input atTop;// IDK IF THIS SHIT IS A REGESTER CAUSE I GOT TO UPDATE THE VALUE BUT IDK How TO DO THAT
+input player;
+output reg[7:0] x;
+output reg[7:0] y;
+
+always@(*moved)
+begin
+	if(atTop == 2b'00)
+	begin
+		y <= givey - 10;
+		if(y == 0)
+		atTop <= 2b'11 
+	
+	end
+	else
+		y <= givey;
+
+	if(player == 2b'11)
+	begin
+		x <= 310;
+	end
+	else
+		x<= 0;
+end
+endmodule 
