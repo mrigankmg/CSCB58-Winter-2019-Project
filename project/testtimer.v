2a8eeb7d2a86e32fc23e1c0a0044a953be4ace9c
prakharS07 <48506991+prakharS07@users.noreply.github.com>
2019-03-29 12:48:56 -0400

    final testversion

--- project/testtimer.v
+++ project/testtimer.v
@@ -22,6 +22,7 @@ module testtimer( clk, HEX0, HEX1, HEX2, SW, done);
 			mins <= {1'b0, SW[8:6]};
 			ones <= 4'b0000;
 			tens <= 4'b0000;
+			done <= 1'b0;
 		end
 		else if(SW[9] == 1'b1) begin
 			if(new_clk == 26'd50000000) begin
@@ -53,28 +54,28 @@ module testtimer( clk, HEX0, HEX1, HEX2, SW, done);
 	end
 endmodule
 
-module hex_decoder(hex_digit, segments);
-	input [3:0] hex_digit;
-	output reg [6:0] segments;
-
-	always @(*)
-		case (hex_digit)
-				4'h0: segments = 7'b100_0000;
-				4'h1: segments = 7'b111_1001;
-            4'h2: segments = 7'b010_0100;
-            4'h3: segments = 7'b011_0000;
-            4'h4: segments = 7'b001_1001;
-            4'h5: segments = 7'b001_0010;
-            4'h6: segments = 7'b000_0010;
-            4'h7: segments = 7'b111_1000;
-            4'h8: segments = 7'b000_0000;
-            4'h9: segments = 7'b001_1000;
-            4'hA: segments = 7'b000_1000;
-            4'hB: segments = 7'b000_0011;
-            4'hC: segments = 7'b100_0110;
-            4'hD: segments = 7'b010_0001;
-            4'hE: segments = 7'b000_0110;
-            4'hF: segments = 7'b000_1110;   
-            default: segments = 7'h7f;
-		endcase
-endmodule
+//module hex_decoder(hex_digit, segments);
+//	input [3:0] hex_digit;
+//	output reg [6:0] segments;
+//
+//	always @(*)
+//		case (hex_digit)
+//				4'h0: segments = 7'b100_0000;
+//				4'h1: segments = 7'b111_1001;
+//            4'h2: segments = 7'b010_0100;
+//            4'h3: segments = 7'b011_0000;
+//            4'h4: segments = 7'b001_1001;
+//            4'h5: segments = 7'b001_0010;
+//            4'h6: segments = 7'b000_0010;
+//            4'h7: segments = 7'b111_1000;
+//            4'h8: segments = 7'b000_0000;
+//            4'h9: segments = 7'b001_1000;
+//            4'hA: segments = 7'b000_1000;
+//            4'hB: segments = 7'b000_0011;
+//            4'hC: segments = 7'b100_0110;
+//            4'hD: segments = 7'b010_0001;
+//            4'hE: segments = 7'b000_0110;
+//            4'hF: segments = 7'b000_1110;   
+//            default: segments = 7'h7f;
+//		endcase
+//endmodule
